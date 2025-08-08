b5f4698dd2d29d058bc7674294516da2fb23dffa
Maddy <m.s.woo2008@gmail.com>
2025-08-08 13:33:45 -0400

    connected content view + new to do view + added animation

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showNewTask = false
     var body: some View {
         VStack {
             HStack{
@@ -16,17 +17,23 @@ struct ContentView: View {
                     .fontWeight(.heavy)
                 Spacer () //pushes text to left
                 Button {
+                    withAnimation {
+                        showNewTask = true
+                    } //end of animation
                 }label: {
-                        Text ("+")
+                    Text ("+")
                         .font (.title)
                         .fontWeight(.bold)
                     } //end of + button
-                
             }//end of HStack
             Spacer ()
             
+            if showNewTask {
+                NewToDoView ()
+            }
         }//end of VStack
         .padding()
+        
     }
 }
 
